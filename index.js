f7d075546785a183650fb8c11f2b2aa432447e60
LanChi <lpham2525@gmail.com>
2020-09-28 16:40:15 -0400

    added in comments explaining the functions and variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// creating variables and assigning npm packages to them
 const { prompt } = require('inquirer')
 const axios = require('axios')
 const shuffle = require('shuffle-array')
@@ -7,11 +8,13 @@ const { promisify } = require('util')
 const wf = promisify(writeFile)
 const rf = promisify(readFile)
 
+// creating variables to be used later
 let categories = []
 let questions = []
 let index = 0
 let points = []
 
+// function to end the game and display the scores
 const endGame = () => {
   console.log(`You got ${points} out of 10 questions right!`)
   prompt({
@@ -20,6 +23,7 @@ const endGame = () => {
     message: 'Please enter a username'
   })
     .then(({ username }) => {
+      // reads the leaderboard for usernames and scores
       rf('leaderboard.json', 'utf-8')
         .then(data => {
           console.log(data)
@@ -28,6 +32,7 @@ const endGame = () => {
             username,
             score: points
           })
+          // writes the username and score into the leaderboard
           wf('leaderboard.json', JSON.stringify(leaderboard))
             .then(() => {
               mainMenu()
@@ -38,36 +43,49 @@ const endGame = () => {
     })
 }
 
+// function for bringing up the next question in the game
 const newQuestion = () => {
   if (index < 10) {
     prompt({
       type: 'list',
       name: 'answer',
       message: questions[index].question,
+      // shuffles the array so that the questions are not always displayed in the same order every time
       choices: shuffle(questions[index].incorrect_answers)
     })
       .then(({ answer }) => {
         if (answer === questions[index].correct_answer) {
+          // alerts the user that they answered correctly
           console.log('correct answer!')
+          // moves the user to the next question until it reaches 10 questions
           index++
+          // increases the score by one point when user answers correctly
           points++
+          // runs newQuestion function to move user to next question
           newQuestion()
         } else {
+          // alerts user that they answered incorrectly
           console.log('incorrect answer!')
+          // reveals the correct answer to the user
           console.log(`The correct answer was ${questions[index].correct_answer}`)
+          // moves index up and displays the next question if index is less than 10
           index++
           newQuestion()
         }
       })
       .catch(err => console.log(err))
   } else {
+    // if index reaches 10, then endGame function is triggered
     endGame()
   }
 }
 
+// function for starting new game
 const newGame = () => {
+  // resets index and points to zero
   index = 0
   points = 0
+  // prompts user to choose a category
   prompt({
     type: 'list',
     name: 'category',
@@ -76,14 +94,17 @@ const newGame = () => {
   })
     .then(({ category }) => {
       let catId = ''
+      // if-else statement to determine if the category id is a single digit or double-digit id
       if (category[1] === ':') {
         catId = category[0]
       } else {
         catId = category.substr(0, 2)
       }
-      axios.get(`https://opentdb.com/api.php?amount=10&category=${catId}&difficulty=easy&type=multiple`)
+      // grabs the category id from the open trivia database
+      axios.get(`https://opentdb.com/api.php?amount=10&category=${catId}&type=multiple`)
         .then(({ data: { results } }) => {
           questions = results.map(question => {
+            // mixes the correct answer with the incorrect answers
             question.incorrect_answers.push(question.correct_answer)
             return question
           })
@@ -94,6 +115,7 @@ const newGame = () => {
     .catch(err => console.log(err))
 }
 
+// function to grab categories from the api if the user chooses to play a new game
 const getCategories = () => {
   if (categories.length < 1) {
     axios.get('https://opentdb.com/api_category.php')
@@ -107,7 +129,9 @@ const getCategories = () => {
   }
 }
 
+// function to view the leaderboard which displays the usernames and scores
 const viewBoard = () => {
+  // reads the leaderboard and sorts the scores from highest to lowest
   rf('leaderboard.json', 'utf8')
     .then(data => {
       let leaderboard = JSON.parse(data)
@@ -117,11 +141,13 @@ const viewBoard = () => {
       leaderboardSorted.forEach(record => {
         console.log(`Username: ${record.username} | Score: ${record.score}`)
       })
+      // returns user to the main menu once they are done viewing the leaderboard
       mainMenu()
     })
     .catch(err => console.log(err))
 }
 
+// function to bring up the main menu when the user enters the trivia game
 const mainMenu = () => {
   prompt({
     type: 'list',
@@ -130,6 +156,7 @@ const mainMenu = () => {
     choices: ['New Game', 'View Leaderboard', 'EXIT']
   })
     .then(({ action }) => {
+      // switch cases to determine the function to run based on user choice
       switch (action) {
         case 'New Game':
           getCategories()
@@ -137,6 +164,7 @@ const mainMenu = () => {
         case 'View Leaderboard':
           viewBoard()
           break
+        // this situation lets the user exit the terminal if they do not want to start a new game or view the leaderboard
         case 'EXIT':
           process.exit()
           break
@@ -145,4 +173,5 @@ const mainMenu = () => {
     .catch(err => console.log(err))
 }
 
+// runs the main menu function once the user enters the terminal
 mainMenu()
